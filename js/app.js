bc969a69ca53254aa47d2fc63a30a16ca0745032
Frank Jacob <post@frank-jacob.me>
2019-02-02 21:19:16 +0100

    Minimized ul in index.html. Filling in cards (one card) programatically.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
-/*
- * Create a list that holds all of your cards
- */
-let array = ['fa-diamond', 'fa-diamond',
+// Create a list that holds all of your cards
+ 
+let setOfCards = ['fa-diamond', 'fa-diamond',
              'fa-paper-plane-o', 'fa-paper-plane-o',
              'fa-anchor', 'fa-anchor',
-             'fa-bolt', 'fa-bolt';
+             'fa-bolt', 'fa-bolt',
              'fa-cube', 'fa-cube',
              'fa-leaf', 'fa-leaf',
              'fa-bicycle', 'fa-bicycle',
              'fa-bomb', 'fa-bomb'];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
+// Display the cards on the page
+
+let deck = document.querySelector('.deck');
+deck.innerHTML = '<li class="card"><i class="fa fa-diamond"></i></li>';
+
+/*   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
@@ -30,6 +32,12 @@ function shuffle(array) {
     }
 
     return array;
+}
+
+// prepare the game and initialize it
+
+function prepareGame() {
+
 }
 
 // set up the event listener for a card. If a card is clicked:
