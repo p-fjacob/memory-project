6acb9cd9fdaebac2b392b79444ec55f119a234e6
Frank Jacob <post@frank-jacob.me>
2019-02-14 17:24:14 +0100

    no change to previous

--- js/app.js
+++ js/app.js
@@ -56,9 +56,10 @@ let moveCounter = 0;
 anyCard.forEach(function(card) {
     card.addEventListener('click', function(openMax2) {
 
-        moveCounter = moveCounter+1;
-        moves.innerText = moveCounter;
-        console.log(moveCounter);
+            moveCounter = moveCounter+1;
+            moves.innerText = moveCounter;
+            console.log(moveCounter);
+
 
         // block clicking an open card twice 
 
@@ -92,6 +93,5 @@ anyCard.forEach(function(card) {
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
